0cd74e09a18b2c00a44120eb41e6ad0436279077
Eric Puidokas <eric@puidokas.com>
2011-09-25 17:32:02 -0700

    fixes

--- src/main/java/com/puidokas/epuidokas/ObGenBlockerBlockListener.java
+++ src/main/java/com/puidokas/epuidokas/ObGenBlockerBlockListener.java
@@ -3,7 +3,6 @@ package com.puidokas.epuidokas;
 import org.bukkit.block.Block;
 import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockListener;
-import org.bukkit.inventory.ItemStack;
 
 public class ObGenBlockerBlockListener extends BlockListener{
     
@@ -14,9 +13,8 @@ public class ObGenBlockerBlockListener extends BlockListener{
             return;
         int source = event.getBlock().getTypeId();
         Block block = event.getToBlock();
-        if ((source == 10 || source == 11 || source == 0) && block.getTypeId() == 55) {
+        if ((source == 11 || source == 0 || source == 10) && block.getTypeId() == 55) {
             block.setTypeId(0);
-            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(331,1));
         }
     }
 }
--- src/main/java/com/puidokas/epuidokas/ObGenBlockerPlugin.java
+++ src/main/java/com/puidokas/epuidokas/ObGenBlockerPlugin.java
@@ -8,9 +8,9 @@ import java.util.logging.Level;
 public class ObGenBlockerPlugin extends JavaPlugin{
     public void onEnable(){
         getServer().getPluginManager().registerEvent(Event.Type.BLOCK_FROMTO, new ObGenBlockerBlockListener(), Event.Priority.Low, this);
-        getServer().getLogger().log(Level.INFO, "[ObGenBlocker] Plugin enabled!");
+        getServer().getLogger().log(Level.INFO, "[" + getDescription().getName() + "] Version " + getDescription().getVersion() + " enabled!");
     }
     public void onDisable(){
-        getServer().getLogger().log(Level.INFO, "[ObGenBlocker] Plugin disabled!");
+        getServer().getLogger().log(Level.INFO, "[" + getDescription().getName() + "] Version " + getDescription().getVersion() + " disabled!");
     }
 }
